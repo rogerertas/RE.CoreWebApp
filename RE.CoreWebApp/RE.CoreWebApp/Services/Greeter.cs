d7a3f253c8af25f8a53736ac485eed403cd87742
Roger Ertas <rogerertas@gmail.com>
2017-06-23 16:57:06 +0200

    Added nugets, refactored code

---
+++ RE.CoreWebApp/RE.CoreWebApp/Services/Greeter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace RE.CoreWebApp.Services
+{
+    public interface IGreeter
+    {
+        string Getmessage();
+    }
+    public class Greeter : IGreeter
+    {
+        private string _message;
+
+        public Greeter(IConfiguration configuration)
+        {
+            _message = configuration["message"];
+        }
+        public string Getmessage()
+        {
+            return _message;
+        }
+    }
+}
